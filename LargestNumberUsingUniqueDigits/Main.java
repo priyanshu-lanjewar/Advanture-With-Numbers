2758a065d2b94d84c945ef96ef8605a2a8817e93
Priyanshu Lanjewar <priyanshu.lanjewar@yahoo.com>
2021-03-25 20:23:38 +0530

    Update and rename adv1.java to Main.java

--- LargestNumberUsingUniqueDigits/Main.java
+++ LargestNumberUsingUniqueDigits/Main.java
@@ -2,7 +2,7 @@ import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.math.BigInteger;
 
-public class adv1
+public class Main
 {
     public static void main(String[] args)  {
         BufferedReader s = new BufferedReader(new InputStreamReader(System.in));
